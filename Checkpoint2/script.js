5eba8da5cd4c462ca274fd8164d30891cb30f316
Vanessa <vnessabaker@gmail.com>
2020-04-13 17:03:33 -0400

    checkpint 2

---
+++ Checkpoint2/script.js
@@ -0,0 +1,13 @@
+const me = "Vee Baker";
+console.log(me);
+
+function sayHi() {
+        console.log("Good afternoon " + me);
+    }
+     sayHi();
+
+
+
+
+    // I was not able to figure out how to apply
+    // method in time, this is how far ive gotten
